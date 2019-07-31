c27762ee1e27a802faaf64c435c07fd0ca451dab
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-07-31 13:56:59 +0700

    mengganti fungsi javascript dengan ES6

---
+++ script.js
@@ -0,0 +1,18 @@
+// Class FILM : representasi semua element yang ada di film
+class Film {
+  constructor(judul, tahun, review) {
+    this.judul = judul;
+    this.tahun = tahun;
+    this.review = review;
+  }
+}
+
+// UI : menghandel user interface / tampilan antarmuka
+
+// Penyimpanan
+
+// Tampilkan Film
+
+// Tambahkan Film
+
+// Hapus Film
