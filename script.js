0d2c55cd33b40a1b8bd2c748e122f418b81d8e97
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-07-31 16:09:16 +0700

    mengubah tampilan tabel

--- script.js
+++ script.js
@@ -8,11 +8,49 @@ class Film {
 }
 
 // UI : menghandel user interface / tampilan antarmuka
+class UI {
+  static tampilFilm() {
+    const simpanFilm = [];
+    const films = simpanFilm;
+
+    films.forEach((i) => UI.tambahFilm(i));
+  }
+  static tambahFilm(i) {
+    const list = document.querySelector('#tbody');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${i.judul}</td>
+    <td>${i.tahun}</td>
+    <td>${i.review}</td>
+    <td><a href="#" class="btn btn-danger btn-sm hapus">x</a></td>
+    `;
+    list.appendChild(row);
+  }
+}
 
 // Penyimpanan
 
 // Tampilkan Film
+document.addEventListener('DOMContentLoaded', UI.tampilFilm);
 
 // Tambahkan Film
+document.querySelector('#film-form').addEventListener('submit', (e) => {
+  
+  // mencegah perilau default dari tombol submit:
+  e.preventDefault();
 
+  // ambil data dari value
+  const judul = document.querySelector('#jf').value;
+  const tahun = document.querySelector('#tf').value;
+  const review = document.querySelector('#rf').value;
+
+  // instatiate flm
+  const film = new Film(judul, tahun, review);
+  console.log(film);
+
+  // Masukkan film ke UI
+  UI.tambahFilm(film);
+
+});
 // Hapus Film
