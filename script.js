2c4bad82a1bcf267f8c1d05402796538c044db29
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-07-31 17:38:43 +0700

    menambahkan fungsi di tombol hapus dan menambahkan alert

--- script.js
+++ script.js
@@ -27,6 +27,26 @@ class UI {
     `;
     list.appendChild(row);
   }
+  static hapusFilm(el) {
+    if(el.classList.contains('hapus')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+  static tampilAlert(pesan, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(pesan));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#form');
+    container.insertBefore(div, form);
+    // hilangkan dalam 2 detik
+    setTimeout(() => document.querySelector('.alert').remove(),3000);
+  }
+  static bersihInput() {
+    document.querySelector('#jf').value = '';
+    document.querySelector('#tf').value = '';
+    document.querySelector('#rf').value = '';
+  }
 }
 
 // Penyimpanan
@@ -45,6 +65,11 @@ document.querySelector('#film-form').addEventListener('submit', (e) => {
   const tahun = document.querySelector('#tf').value;
   const review = document.querySelector('#rf').value;
 
+  // Validasi data input
+  if(judul === '' || tahun === '' || review === '') {
+    UI.tampilAlert('Lengkapi form yang ada!', 'danger');
+  } else {
+
   // instatiate flm
   const film = new Film(judul, tahun, review);
   console.log(film);
@@ -52,5 +77,18 @@ document.querySelector('#film-form').addEventListener('submit', (e) => {
   // Masukkan film ke UI
   UI.tambahFilm(film);
 
+  // Tampil Sukses
+  UI.tampilAlert('Film berhasil ditambahkan', 'success');
+
+  // Bersihkan input
+  UI.bersihInput();
+  }
 });
+
 // Hapus Film
+document.querySelector('#tbody').addEventListener('click', (e) => {
+  UI.hapusFilm(e.target);
+
+  // Tampil berhasil dihapus
+  UI.tampilAlert('Film berhasil dihapus', 'warning');
+});
